a93dcf84f562f447d47070bb7f2ad7fa86a7fa61
anita <nitinhaanjos@gmail.com>
2024-07-12 11:30:39 -0300

    TESTE DE OPERAÇÕES

---
+++ myProject/Mycals.py
@@ -0,0 +1,15 @@
+#demonstração do uso de operadores
+X = int(input("digite um numero: "))
+Y = int(input("digite outro numero:"))
+
+SOMA = X + Y 
+print("o resultado da operação é:", SOMA)
+
+SUBTRAÇÃO = X - Y 
+print("o resultado da subtração é:", SUBTRAÇÃO)
+
+MULTIPLICAÇÃO = X * Y 
+print("o resultado da multiplicação é", MULTIPLICAÇÃO)
+
+DIVISÃO = X / Y 
+print("o resultado da divisão é", DIVISÃO)
